8e54ac6601203bfe8e5c17caee172fbf165bbd6d
Ahmed Ashraf <ahmed.bnelashraf@gmail.com>
2026-01-17 23:56:05 +0200

    Configure firebase & adding  done

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruit_hub/core/helper_functoins/on_generate_routes.dart';
 import 'package:fruit_hub/core/services/prefs.dart';
 import 'package:fruit_hub/core/utils/app_colors.dart';
 import 'package:fruit_hub/features/splash/presentation/views/splash_view.dart';
+import 'package:fruit_hub/firebase_options.dart';
 import 'package:fruit_hub/generated/l10n.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Prefs.init();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const FruitHub());
 }
