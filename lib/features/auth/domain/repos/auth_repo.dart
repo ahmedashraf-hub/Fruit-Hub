d0deb6a8e37c804213cc19b61bcede4a30ee5e2f
Ahmed Ashraf <ahmed.bnelashraf@gmail.com>
2026-01-19 17:08:13 +0200

    user entity done, add dartz package

--- lib/features/auth/domain/repos/auth_repo.dart
+++ lib/features/auth/domain/repos/auth_repo.dart
@@ -1,1 +1,10 @@
-abstract class AuthRepo {}
+import 'package:dartz/dartz.dart';
+import 'package:fruit_hub/core/errors/failures.dart';
+import 'package:fruit_hub/features/auth/domain/entities/user_entity.dart';
+
+abstract class AuthRepo {
+  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword(
+    String email,
+    String password,
+  );
+}
