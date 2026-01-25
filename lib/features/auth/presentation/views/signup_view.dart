5e35a1a61ade3762dfaf030bcace56cf10e914cb
Ahmed Ashraf <ahmed.bnelashraf@gmail.com>
2026-01-25 15:50:54 +0200

    Integrate Signup cubit

--- lib/features/auth/presentation/views/signup_view.dart
+++ lib/features/auth/presentation/views/signup_view.dart
@@ -4,7 +4,7 @@ import 'package:fruit_hub/core/services/get_it_service.dart';
 import 'package:fruit_hub/core/widgets/custom_app_bar.dart';
 import 'package:fruit_hub/features/auth/domain/repos/auth_repo.dart';
 import 'package:fruit_hub/features/auth/presentation/views/cubits/signup_cubits/signup_cubit.dart';
-import 'package:fruit_hub/features/auth/presentation/views/widgets/signup_view_body.dart';
+import 'package:fruit_hub/features/auth/presentation/views/widgets/signup_view_body_bloc_consumer.dart';
 
 class SignupView extends StatelessWidget {
   const SignupView({super.key});
@@ -17,7 +17,11 @@ class SignupView extends StatelessWidget {
       create: (context) => SignupCubit(getIt<AuthRepo>()),
       child: Scaffold(
         appBar: buildAppBar(context, title: 'حساب جديد'),
-        body: const SignupViewBody(),
+        body: Builder(
+          builder: (context) {
+            return const SignupViewBodyBlocConsumer();
+          },
+        ),
       ),
     );
   }
---
+++ lib/features/auth/presentation/views/widgets/signup_view_body_bloc_consumer.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruit_hub/features/auth/presentation/views/cubits/signup_cubits/signup_cubit.dart';
+import 'package:fruit_hub/features/auth/presentation/views/widgets/signup_view_body.dart';
+
+class SignupViewBodyBlocConsumer extends StatelessWidget {
+  const SignupViewBodyBlocConsumer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<SignupCubit, SignupState>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        return const SignupViewBody();
+      },
+    );
+  }
+}
